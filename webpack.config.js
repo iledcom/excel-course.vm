de311e1e9b051a394fe298e043c07513ab92d11a
Konstantyn <korenev@iled.com.ua>
2021-10-15 15:57:24 +0300

    customization loader

--- webpack.config.js
+++ webpack.config.js
@@ -36,5 +36,17 @@ module.exports = {
         new MiniCssExtractPlugin({
             filename: 'bundle.[hash].css'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader",
+                "sass-loader",
+              ],
+            },
+          ],
+    }
 }
