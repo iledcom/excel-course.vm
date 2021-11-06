3d928fe556fb8c806603e1554663df948e831445
Konstantyn Korenev <korenev@iled.com.ua>
2021-11-06 19:20:12 +0200

    add dom.js, class Dom, function $

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,12 +1,25 @@
+import {$} from '@core/dom'
+
+
 export class Excel {
      constructor(selector, options) {
          this.$el = document.querySelector(selector)
          this.components = options.components || []
      }
 
+     getRoot() {
+         const $root = $.create('div', 'excel')
+
+         this.components.forEach(Component => {
+             const $el = $.create('div', Component.className)
+             const component = new Component($el)
+             $el.innerHTML = component.toHTML()
+             $root.append($el)
+         })
+         return $root
+     }
+
      render() {
-         // afterbegin, afterend, beforeend, beforebegin
-         this.$el.insertAdjacentHTML('afterbegin', `<h1>Test</h1>`)
-         console.log('Test');
+         this.$el.append(this.getRoot())
      }
 }
