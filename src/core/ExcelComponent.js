e1af2327788977f28259cc138a43de07be679fbf
Konstantyn <korenev@iled.com.ua>
2022-01-25 16:58:10 +0200

    03_08_Добавление прослушки событий

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,8 +1,16 @@
 import {DomListener} from '@core/DomListener';
 
 export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.lesteners)
+        this.name = options.name || ''
+    }
     // Возвращает шаблон компонента
     toHTML() {
         return ''
+    }
+
+    init() {
+        this.initDOMListeners()
     }
 }
