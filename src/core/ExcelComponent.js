4ac3595981cda039a786ac8ecff875399d7d2b80
Konstantyn <korenev@iled.com.ua>
2022-01-28 15:49:17 +0200

    Framework start (#3)
    * Creating a class structure
    
    * add change to Excel.js
    
    * excel.render
    
    * add dom.js, class Dom, function $
    
    * 03_08_Добавление прослушки событий
    
    * 3.11
    
    * finish base framework functional

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,20 @@
+import {DomListener} from '@core/DomListener';
+
+export class ExcelComponent extends DomListener {
+    constructor($root, options = {}) {
+        super($root, options.lesteners)
+        this.name = options.name || ''
+    }
+    // Возвращает шаблон компонента
+    toHTML() {
+        return ''
+    }
+
+    init() {
+        this.initDOMListeners()
+    }
+
+    destroy() {
+        this.removeDOMListeners()
+    }
+}
